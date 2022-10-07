7176e8755be9a8d64e1facf43e6f053fdc13f18f
Rie Ono <ooneri0122@gmail.com>
2022-10-07 14:19:01 +0900

    Update Analyze.sql

--- Analyze.sql
+++ Analyze.sql
@@ -1,17 +1,15 @@
-# ===== STEP1 ======
-# Count number of ride_id by casual vs member when ride_length_h > 0 
+# STEP1
+# Counted number of ride ids : member vs casual
 
 SELECT 
   COUNT (ride_id) 
-FROM tripdata
-WHERE ride_length_h > 0
-GROUP BY member_casual
+FROM 
+`sturdy-pier-354508.Cousera_Cyclistic.12months without null`
+GROUP BY user_type
 
-
-
-# ===== STEP2 ======
-# Count top3 ride length and each number of rides. 
-# Besides, calcurated avg, max and min ride lengths. 
+# STEP2
+# Counted top 3 ride lengths and each number of rides. 
+# Calcurated avg, max and min ride length per member or casual. 
 
 SELECT
   AVG(ride_length_h),
@@ -19,80 +17,32 @@ SELECT
   MIN(ride_length_h),
   APPROX_TOP_COUNT(ride_length_h,3)
 FROM
-tripdata
-WHERE ride_length_h > 0
-GROUP BY member_casual
+`sturdy-pier-354508.Cousera_Cyclistic.12months without null`
+GROUP BY user_type
 
-
-
-# ===== STEP3 =====
-# Count number of rides by day of the week.
+# STEP3
+# Counted number of rides: member vs casual by day of the week and per hour.
 
 SELECT
-  COUNTIF(member_casual = 'casual') AS casual,
-  COUNTIF(member_casual = 'member') AS member,
-FROM
-tripdata
-WHERE ride_length_h > 0
-GROUP BY day_of_week
+ TIME_TRUNC(started_time,HOUR) AS started_hour,
+ COUNTIF(user_type = 'member') AS member,
+ COUNTIF(user_type = 'casual') AS casual
+FROM ( SELECT 
+ user_type,
+ day_of_week,
+ TIME(started_at) AS started_time
+FROM `sturdy-pier-354508.Cousera_Cyclistic.12months without null` )
+GROUP BY day_of_week,started_hour
+ORDER BY day_of_week,started_hour
 
-
-
-# ===== STEP4 =====
-# I found that casual riders are less than members on weekdays　on STEP3.
-#　I expected that office workers ride bikes for commuting.
-#　Thus I extracted only time data from timestamp and saved it with some necessary columns in a temporary table. 
-
-SELECT
-　　member_casual,
-　　day_of_week,
-　　ride_length_h,
-TIME(started_at) AS started_time
-FROM
-tripdata
-WHERE ride_length_h > 0
-
-
-# ===== STEP5 =====
-# Count each number of rides both between 5am-10am and 4pm-7pm by member vs casual.
+# STEP4
+# Counted number of rides: member vs casual by month.
 
 SELECT 
-  COUNTIF(member_casual = 'member') AS member,
-  COUNTIF(member_casual = 'casual') AS casual
-FROM tripdata
-WHERE
-started_time BETWEEN "05:00:00" AND "10:00:00"
-GROUP BY day_of_week
-
-
-SELECT 
-  COUNTIF(member_casual = 'member') AS member,
-  COUNTIF(member_casual = 'casual') AS casual
-FROM tripdata
-WHERE
-started_time BETWEEN "16:00:00" AND "19:00:00"
-GROUP BY day_of_week
-
-
-
-# ===== STEP6 =====
-# count number of rides per each type of bike. 
-
-
-SELECT
-  COUNTIF(member_casual = 'member') AS member,
-  COUNTIF(member_casual = 'casual') AS casual
-FROM tripdata
-GROUP BY rideable_type 
-
-
-
-# ===== STEP7 ======
-# Count monthly member vs casual riders from monthly table I created in prior.
-
-SELECT 
-  COUNTIF ( member_casual = 'casual') AS casual,
-  COUNTIF ( member_casual = 'member') AS member,
-FROM monthly-trip-data
-WHERE ride_length_h > 0
+  COUNTIF ( user_type = 'casual') AS casual,
+  COUNTIF ( user_type = 'member') AS member,
+FROM ( SELECT 
+user_type,
+EXTRACT ( MONTH FROM started_at) AS MONTH
+FROM `sturdy-pier-354508.Cousera_Cyclistic.12months without null` )
 GROUP BY MONTH
